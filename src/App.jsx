84f7a73e8c2cc175a0f0f4fbdb19963d605a03fd
yousseforabi <122828573+yousseforabi@users.noreply.github.com>
2024-12-06 02:46:40 +0100

    Structure the projects

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BookSearch from './pages/index'
+
+function App() {
+  return (
+    <div className="App">
+      <BookSearch />
+    </div>
+  )
+}
+
+export default App
